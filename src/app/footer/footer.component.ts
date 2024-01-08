0fac850f0d31b0fcad4b33997db35a7581979081
David Alvarado <davidalvarado2003@hotmail.com>
2024-01-07 23:22:35 -0500

    Agregación del footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  paginas = [
+    {path: 'paginas/inicio', title:'Inicio'},
+    {path: 'paginas/acercade', title: 'Acerca de'},
+    {path: 'paginas/categorias', title: 'Categorias'},
+    {path: 'paginas/registrarse', title: 'Registrarse'},
+    {path: 'paginas/iniciarSesion', title: 'Iniciar sesion'}
+  ]
 }
