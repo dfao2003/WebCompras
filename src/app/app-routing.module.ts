2466b2f6f0e3a23cf6bb0733aefd37097676afed
David Alvarado <davidalvarado2003@hotmail.com>
2024-01-20 18:37:34 -0500

    Cambios para el merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { CategoriasComponent } from './pages/categorias/categorias.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
-import { RegisterComponent } from './form/register/register.component';
-import { SignupComponent } from './form/signup/signup.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 
 const routes: Routes = [
   {path: 'paginas/inicio', component: InicioComponent },
   {path: 'paginas/categorias', component: CategoriasComponent},
   {path: 'paginas/acercade', component: AcercadeComponent},
-  {path: 'paginas/registrarse', component: RegisterComponent},
-  {path: 'paginas/iniciarSesion', component: SignupComponent},
   {path: 'paginas/producto', component: ProductoComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { CategoriasComponent } from './pages/categorias/categorias.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
-import { SignupComponent } from './form/signup/signup.component';
-import { RegisterComponent } from './form/register/register.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,8 +21,6 @@ import { FormsModule } from '@angular/forms';
     MenuComponent,
     CategoriasComponent,
     CarritoComponent,
-    SignupComponent,
-    RegisterComponent,
     AcercadeComponent,
     FooterComponent,
     HeaderComponent,
